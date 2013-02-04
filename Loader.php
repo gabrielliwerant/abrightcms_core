29b30c0025e0b663079a021d3d6571d4e0def570
Gabriel <gabriel@abrightconcept.com>
2013-02-04 04:19:50 -0500

    Refactor Loader class to use more methods for better reusability and to allow searching through subdirectories in addition to main directories

--- Loader.php
+++ Loader.php
@@ -21,6 +21,11 @@
 class Loader
 {
 	/**
+	 * Error codes for Loader
+	 */
+	const FILE_PATH_COULD_NOT_BE_OPENED		= 1001;
+	
+	/**
 	 * Stored paths for use in our autoloader method.
 	 *
 	 * @var array $path
@@ -48,35 +53,153 @@ class Loader
 	}
 	
 	/**
-	 * Set additional paths for our autoloader to call upon. Useful for 
-	 * assigning conditional paths after construction, like development paths.
+	 * Validate that our file path is accurate.
 	 *
-	 * @param string $path 
+	 * @param string $file_path
+	 * 
+	 * @return boolean 
 	 */
-	public function setAdditionalPath($path)
+	public static function isFilePathValid($file_path)
 	{
-		self::$path[] = $path;
+		if (file_exists($file_path))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	/**
+	 * Open a file path and look through it for appropriate subdirectories.
+	 *
+	 * @param string $path
+	 * 
+	 * @return array Return null if no appropriate directories were found
+	 */
+	public static function getSubdirectoryArrayFromFilePath($path)
+	{
+		$handle	= opendir($path);
+		
+		if ($handle)
+		{
+			// Must use strict comparison to false or we have silent issues
+			while (($directory = readdir($handle)) !== false)
+			{
+				$has_period = strstr($directory, '.');
+				$is_license	= strstr($directory, 'LICENSE');
+				
+				// We don't want anything with a period, as it's either a single
+				// file or the period only directories.
+				if ( ! $has_period AND ! $is_license)
+				{
+					$directory_arr[] = $directory;
+				}
+			}
+
+			closedir($handle);
+			
+			if (isset($directory_arr))
+			{
+				return $directory_arr;
+			}
+			else
+			{
+				return null;
+			}
+		}
+		else
+		{
+			throw ApplicationFactory::makeException('Loader Exception', self::FILE_PATH_COULD_NOT_BE_OPENED);
+			//throw new Exception('Loader Exception', self::FILE_PATH_COULD_NOT_BE_OPENED);
+		}
+	}
+	
+	/**
+	 * From an array of directories listed outermost to innermost, built the
+	 * appropriate file path for our class name.
+	 *
+	 * @param array $directory_arr
+	 * @param string $file_name
+	 * 
+	 * @return string 
+	 */
+	public static function buildFilePath($directory_arr, $file_name)
+	{
+		$path = null;
+		
+		foreach ($directory_arr as $directory)
+		{
+			$path .= $directory . '/';
+		}
+
+		return $path . $file_name . '.php';
+	}
+	
+	/**
+	 * Load a file after we've built the proper path and checked whether or not
+	 * it exists.
+	 *
+	 * @param array $directory_arr
+	 * @param string $file_name 
+	 */
+	public static function load($directory_arr, $file_name)
+	{
+		$file_path = self::buildFilePath($directory_arr, $file_name);
+
+		if (self::isFilePathValid($file_path))
+		{
+			require $file_path;
+		}
 	}
 	
 	/**
 	 * The method to find the proper paths for autoloading our classes.
 	 * 
-	 * We run through the list of paths given in the class property and build 
-	 * the full class path from those.
+	 * We run through the list of paths given in the class property, check
+	 * subdirectories of those, and attempt to build the full class path to load.
 	 * 
-	 * @param type $class_name The class to load
+	 * @param string $class_name
+	 * 
+	 * @todo handle directories in recursive fashion so that we are not limited
+	 *		to a single subdirectory when searching.
 	 */
 	public static function autoload($class_name)
 	{
-		foreach (self::$path as $value)
+		foreach (self::$path as $main_dir)
 		{
-			$file_path = $value . '/' . $class_name . '.php';
-			
-			if (file_exists($file_path))
+			if ($main_dir['search_sub_dir'])
 			{
-				require $file_path;
+				$sub_dir_arr = self::getSubdirectoryArrayFromFilePath($main_dir['path']);
+
+				if ( ! empty($sub_dir_arr))
+				{			
+					foreach ($sub_dir_arr as $sub_dir)
+					{
+						self::load(array($main_dir['path'], $sub_dir), $class_name);
+					}
+				}
 			}
+			
+			self::load(array($main_dir['path']), $class_name);
 		}
+	}
+	
+	/**
+	 * Set additional paths for our autoloader to call upon. Useful for 
+	 * assigning conditional paths after construction, like development paths.
+	 *
+	 * @param string $key
+	 * @param string $path 
+	 * 
+	 * return object Loader
+	 */
+	public function setAdditionalPath($key, $path)
+	{
+		self::$path[$key] = $path;
+		
+		return $this;
 	}
 }
 // End of Loader Class
